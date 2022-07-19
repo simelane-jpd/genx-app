5fd0fe48276cad782147f9b323caf4bd237a1ba7
simelane-jpd <201575227@student.uj.ac.za>
2022-07-19 05:52:04 +0200

    app changes

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+//import javascriptLogo from './javascript.svg'
+///import { setupCounter } from './counter.js'
+import './shop.js'
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine
+Alpine.start()
+//setupCounter(document.querySelector('#counter'))
